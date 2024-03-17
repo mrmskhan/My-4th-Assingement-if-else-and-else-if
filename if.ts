3701ce842c58946e2ad6939cce95f736f9c2d89a
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2024-03-18 02:28:03 +0500

    Add files via upload

---
+++ if.ts
@@ -0,0 +1,35 @@
+// create login system using if else & else if else if else if
+
+let usename:string= "Maria";
+let password:string= "9090";
+let code:string="8080"
+if (usename == "Saleem" && password == "12345"){
+    console.log("kindly provide code on your mobile")
+    if(code == "786"){
+        console.log("logedin"+" welcome "+usename)
+    }
+    else{
+        console.log("invalid code")
+    }
+}
+else if(usename == "Mustafa" && password == "123"){
+    console.log("kindly provide code on your mobile")
+    if( code == "2233"){
+        console.log("logedin"+" welcome "+usename)
+    }
+    else{
+        console.log("invalid code")
+    }
+}
+else if(usename == "Maria" && password == "9090"){
+    console.log("kindly provide code on your mobile")
+    if(code == "8080"){
+        console.log("logedin"+" welcome "+usename)
+    }
+    else{
+        console.log("invalid code")
+    }
+}
+else{
+    console.log("incorrect username or password")
+}
